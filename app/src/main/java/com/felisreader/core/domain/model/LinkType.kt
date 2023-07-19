555ec867c0ab4f5e8c9263716cfd44897b0af1a1
Ferdinand <nandoes_0218@hotmail.com>
2023-07-18 19:50:03 -0600

    Add branded icons to external links

--- app/src/main/java/com/felisreader/core/domain/model/LinkType.kt
+++ app/src/main/java/com/felisreader/core/domain/model/LinkType.kt
@@ -2,15 +2,15 @@ package com.felisreader.core.domain.model
 
 sealed class LinkType(val key: String, val relatedSite: String, val url: String) {
     data class AL(val id: String): LinkType("al", "AniList", "https://anilist.co/manga/${id}")
-    data class AP(val slug: String): LinkType("ap", "Anime Planet", "https://www.anime-planet.com/manga/${slug}")
+    data class AP(val slug: String): LinkType("ap", "Anime-Planet", "https://www.anime-planet.com/manga/${slug}")
     data class BW(val slug: String): LinkType("bw", "Book Walker", "https://bookwalker.jp/${slug}")
-    data class MU(val id: String): LinkType("mu", "Manga Updates", "https://www.mangaupdates.com/series.html?id=${id}")
-    data class NU(val slug: String): LinkType("nu", "Novel Updates", "https://www.novelupdates.com/series/${slug}")
+    data class MU(val id: String): LinkType("mu", "MangaUpdates", "https://www.mangaupdates.com/series.html?id=${id}")
+    data class NU(val slug: String): LinkType("nu", "NovelUpdates", "https://www.novelupdates.com/series/${slug}")
     //data class KT(val id: String): LinkType("kt", "kitsu.io", "https://kitsu.io/api/edge/manga/${id}") // TODO: Slug
     data class AMZ(val siteUrl: String): LinkType("amz", "Amazon", siteUrl)
-    data class EBJ(val siteUrl: String): LinkType("ebj", "eBook Japan", siteUrl)
-    data class MAL(val id: String): LinkType("mal", "My Anime List", "https://myanimelist.net/manga/${id}")
+    data class EBJ(val siteUrl: String): LinkType("ebj", "eBookJapan", siteUrl)
+    data class MAL(val id: String): LinkType("mal", "MyAnimeList", "https://myanimelist.net/manga/${id}")
     data class CDJ(val siteUrl: String): LinkType("cdj", "CDJapan", siteUrl)
-    data class RAW(val siteUrl: String): LinkType("raw", "Raw", siteUrl)
-    data class ENGTL(val siteUrl: String): LinkType("engtl", "Seven Seas Ent.", siteUrl)
+    data class RAW(val siteUrl: String): LinkType("raw", "Official Raw", siteUrl)
+    data class ENGTL(val siteUrl: String): LinkType("engtl", "Official English", siteUrl)
 }
--- app/src/main/java/com/felisreader/manga/presentation/manga_info/MangaScreen.kt
+++ app/src/main/java/com/felisreader/manga/presentation/manga_info/MangaScreen.kt
@@ -5,14 +5,18 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Language
 import androidx.compose.material.icons.outlined.OpenInNew
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.felisreader.R
 import com.felisreader.core.domain.model.ContentRating
 import com.felisreader.core.presentation.TagChipGroup
 import com.felisreader.manga.domain.model.Manga
@@ -113,19 +117,79 @@ fun InfoTabs(
             }
             1 -> {
                 FlowRow(
-                    horizontalArrangement = Arrangement.Start,
-                    verticalArrangement = Arrangement.Top
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalArrangement = Arrangement.Top,
+                    maxItemsInEachRow = 2
                 ) {
                     manga.links.forEach {
                         if (it != null) {
                             AssistChip(
-                                modifier = Modifier.padding(5.dp),
                                 label = { Text(text = it.relatedSite) },
                                 onClick = {
                                     uriHandler.openUri(it.url)
                                 },
+                                leadingIcon = {
+                                    when (it.key) {
+                                        "al" -> Icon(
+                                            modifier = Modifier.size(24.dp),
+                                            painter = painterResource(id = R.drawable.al_icon),
+                                            contentDescription = "Anime List Icon",
+                                            tint = Color.Unspecified
+                                        )
+                                        "ap" -> Icon(
+                                            modifier = Modifier.size(24.dp),
+                                            painter = painterResource(id = R.drawable.ap_icon),
+                                            contentDescription = "Anime Planet Icon",
+                                            tint = Color.Unspecified
+                                        )
+                                        "bw" -> Icon(
+                                            modifier = Modifier.size(24.dp),
+                                            painter = painterResource(id = R.drawable.bw_icon),
+                                            contentDescription = "Book Walker Icon",
+                                            tint = Color.Unspecified
+                                        )
+                                        "mu" -> Icon(
+                                            modifier = Modifier.size(24.dp),
+                                            painter = painterResource(id = R.drawable.mu_icon),
+                                            contentDescription = "Book Walker Icon",
+                                            tint = Color.Unspecified
+                                        )
+                                        "amz" -> Icon(
+                                            modifier = Modifier.size(24.dp),
+                                            painter = painterResource(id = R.drawable.amz_icon),
+                                            contentDescription = "Amazon Icon",
+                                            tint = Color.Unspecified
+                                        )
+                                        "ebj" -> Icon(
+                                            modifier = Modifier.size(24.dp),
+                                            painter = painterResource(id = R.drawable.ebj_icon),
+                                            contentDescription = "eBook Japan Icon",
+                                            tint = Color.Unspecified
+                                        )
+                                        "mal" -> Icon(
+                                            modifier = Modifier.size(24.dp),
+                                            painter = painterResource(id = R.drawable.mal_icon),
+                                            contentDescription = "My Anime List Icon",
+                                            tint = Color.Unspecified
+                                        )
+                                        "cdj" -> Icon(
+                                            modifier = Modifier.size(24.dp),
+                                            painter = painterResource(id = R.drawable.cdj_icon),
+                                            contentDescription = "CD Japan Icon",
+                                            tint = Color.Unspecified
+                                        )
+                                        else -> Icon(
+                                            modifier = Modifier.size(24.dp),
+                                            imageVector = Icons.Outlined.Language,
+                                            contentDescription = "Website Icon",
+                                            tint = MaterialTheme.colorScheme.outline
+                                        )
+                                    }
+                                },
                                 trailingIcon = {
                                     Icon(
+                                        modifier = Modifier.size(12.dp),
                                         imageVector = Icons.Outlined.OpenInNew,
                                         contentDescription = "Open in new icon"
                                     )
