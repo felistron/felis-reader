49016e81c233571f7ae63b2fc80219dc52b52a44
Fernando Espinosa <jferespinosa18@gmail.com>
2025-01-16 15:15:12 -0600

    Hide BottomBar at LectorScreen

--- app/src/main/java/com/felisreader/core/presentation/BottomBar.kt
+++ app/src/main/java/com/felisreader/core/presentation/BottomBar.kt
@@ -1,5 +1,6 @@
 package com.felisreader.core.presentation
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.LibraryBooks
@@ -27,80 +28,84 @@ fun BottomBar(
     val navBackStackEntry by navController.currentBackStackEntryAsState()
     val currentRoute = navBackStackEntry?.destination?.route
 
-    NavigationBar(
-        modifier = modifier,
-        containerColor = MaterialTheme.colorScheme.background,
-        contentColor = MaterialTheme.colorScheme.onBackground,
-        tonalElevation = 0.dp,
-    ) {
-        NavigationBarItem(
-            icon = {
-                Icon(
-                    imageVector = Icons.Filled.Home,
-                    contentDescription = null,
-                )
-            },
-            label = {
-                Text(stringResource(id = R.string.ui_home))
-            },
-            selected = currentRoute == Screen.HomeScreen().route,
-            onClick = {
-                navController.navigate(Screen.HomeScreen().route) {
-                    navController.graph.startDestinationRoute?.let { route ->
-                        popUpTo(route) {
-                            saveState = true
+    val showNavigationBar = currentRoute != Screen.LectorScreen().route
+
+    AnimatedVisibility(showNavigationBar) {
+        NavigationBar(
+            modifier = modifier,
+            containerColor = MaterialTheme.colorScheme.background,
+            contentColor = MaterialTheme.colorScheme.onBackground,
+            tonalElevation = 0.dp,
+        ) {
+            NavigationBarItem(
+                icon = {
+                    Icon(
+                        imageVector = Icons.Filled.Home,
+                        contentDescription = null,
+                    )
+                },
+                label = {
+                    Text(stringResource(id = R.string.ui_home))
+                },
+                selected = currentRoute == Screen.HomeScreen().route,
+                onClick = {
+                    navController.navigate(Screen.HomeScreen().route) {
+                        navController.graph.startDestinationRoute?.let { route ->
+                            popUpTo(route) {
+                                saveState = true
+                            }
                         }
+                        launchSingleTop = true
+                        restoreState = true
                     }
-                    launchSingleTop = true
-                    restoreState = true
-                }
-            },
-        )
-        NavigationBarItem(
-            icon = {
-                Icon(
-                    imageVector = Icons.Filled.Search,
-                    contentDescription = null,
-                )
-            },
-            label = {
-                Text(stringResource(id = R.string.ui_search))
-            },
-            selected = currentRoute == Screen.SearchScreen().route,
-            onClick = {
-                navController.navigate(Screen.SearchScreen().route) {
-                    navController.graph.startDestinationRoute?.let { route ->
-                        popUpTo(route) {
-                            saveState = true
+                },
+            )
+            NavigationBarItem(
+                icon = {
+                    Icon(
+                        imageVector = Icons.Filled.Search,
+                        contentDescription = null,
+                    )
+                },
+                label = {
+                    Text(stringResource(id = R.string.ui_search))
+                },
+                selected = currentRoute == Screen.SearchScreen().route,
+                onClick = {
+                    navController.navigate(Screen.SearchScreen().route) {
+                        navController.graph.startDestinationRoute?.let { route ->
+                            popUpTo(route) {
+                                saveState = true
+                            }
                         }
+                        launchSingleTop = true
+                        restoreState = true
                     }
-                    launchSingleTop = true
-                    restoreState = true
                 }
-            }
-        )
-        NavigationBarItem(
-            icon = {
-                Icon(
-                    imageVector = Icons.Filled.LibraryBooks,
-                    contentDescription = null,
-                )
-            },
-            label = {
-                Text(stringResource(id = R.string.ui_library))
-            },
-            selected = currentRoute == Screen.LibraryScreen().route,
-            onClick = {
-                navController.navigate(Screen.LibraryScreen().route) {
-                    navController.graph.startDestinationRoute?.let { route ->
-                        popUpTo(route) {
-                            saveState = true
+            )
+            NavigationBarItem(
+                icon = {
+                    Icon(
+                        imageVector = Icons.Filled.LibraryBooks,
+                        contentDescription = null,
+                    )
+                },
+                label = {
+                    Text(stringResource(id = R.string.ui_library))
+                },
+                selected = currentRoute == Screen.LibraryScreen().route,
+                onClick = {
+                    navController.navigate(Screen.LibraryScreen().route) {
+                        navController.graph.startDestinationRoute?.let { route ->
+                            popUpTo(route) {
+                                saveState = true
+                            }
                         }
+                        launchSingleTop = true
+                        restoreState = true
                     }
-                    launchSingleTop = true
-                    restoreState = true
                 }
-            }
-        )
+            )
+        }
     }
 }
