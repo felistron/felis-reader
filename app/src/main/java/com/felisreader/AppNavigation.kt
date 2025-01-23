ec3eefb68d1b9e970571a2cd58a9c79e67baa93d
Fernando Espinosa <jferespinosa18@gmail.com>
2025-01-23 15:33:22 -0600

    Get stringResource to history label

--- app/src/main/java/com/felisreader/AppNavigation.kt
+++ app/src/main/java/com/felisreader/AppNavigation.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -206,7 +207,7 @@ fun AppNavigation(
             }
 
             setTopBarTitle {
-                Text("History")
+                Text(stringResource(id = R.string.ui_library_manga_history))
             }
 
             MangaHistoryScreen(
