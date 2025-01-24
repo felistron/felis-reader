c6a2875c4a390d7051dfff66ed33525834101d5f
Fernando Espinosa <jferespinosa18@gmail.com>
2025-01-24 12:51:34 -0600

    Fix #72

--- app/src/main/java/com/felisreader/chapter/presentation/chapter_lector/components/PageImage.kt
+++ app/src/main/java/com/felisreader/chapter/presentation/chapter_lector/components/PageImage.kt
@@ -3,6 +3,7 @@ package com.felisreader.chapter.presentation.chapter_lector.components
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
 import com.felisreader.R
@@ -17,6 +18,7 @@ fun PageImage(
         modifier = Modifier.fillMaxWidth(),
         model = imageUrl,
         contentDescription = null,
+        contentScale = ContentScale.FillWidth,
     ) {
         it.placeholder(R.drawable.manga_cover)
     }
