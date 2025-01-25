3df23ed7d6ec3fb20511439838abc1ab664dfcdf
Fernando Espinosa <jferespinosa18@gmail.com>
2025-01-25 15:15:27 -0600

    Fix #75

--- app/src/main/java/com/felisreader/library/presentation/manga_history/MangaHistoryScreen.kt
+++ app/src/main/java/com/felisreader/library/presentation/manga_history/MangaHistoryScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -22,8 +23,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.felisreader.R
 import com.felisreader.core.presentation.Loading
 import com.felisreader.manga.presentation.manga_search.components.MangaCard
 
@@ -53,54 +56,67 @@ fun MangaHistoryContent(
 ) {
     val scrollState by remember { mutableStateOf(LazyListState()) }
 
-    if (state.history == null) {
-        Loading(
-            modifier = Modifier.fillMaxSize(),
-            size = 64
-        )
-    } else {
-        LazyColumn(
-            modifier = Modifier
-                .padding(horizontal = 16.dp)
-                .fillMaxSize(),
-            verticalArrangement = Arrangement.spacedBy(16.dp),
-            state = scrollState
-        ) {
-            item {
-                Spacer(modifier = Modifier.height(8.dp))
-            }
+    when {
+        state.history == null && state.canLoadMore -> {
+            Loading(
+                modifier = Modifier.fillMaxSize(),
+                size = 64
+            )
+        }
 
-            items(
-                items = state.history.data,
-                key = { it.id }
-            ) { manga ->
-                Box(
-                    contentAlignment = Alignment.TopEnd
-                ) {
-                    MangaCard(
-                        manga = manga,
-                        onCardClick = { navigateToManga(manga.id) },
-                    )
-                    IconButton(
-                        onClick = { onEvent(MangaHistoryEvent.DeleteHistoryItem(manga.id)) },
+        state.history == null && !state.canLoadMore -> {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center,
+            ) {
+                Text(stringResource(id = R.string.ui_empty_result))
+            }
+        }
+
+        state.history != null -> {
+            LazyColumn(
+                modifier = Modifier
+                    .padding(horizontal = 16.dp)
+                    .fillMaxSize(),
+                verticalArrangement = Arrangement.spacedBy(16.dp),
+                state = scrollState
+            ) {
+                item {
+                    Spacer(modifier = Modifier.height(8.dp))
+                }
+
+                items(
+                    items = state.history.data,
+                    key = { it.id }
+                ) { manga ->
+                    Box(
+                        contentAlignment = Alignment.TopEnd
                     ) {
-                        Icon(
-                            Icons.Filled.Delete,
-                            contentDescription = null,
-                            tint = MaterialTheme.colorScheme.error
+                        MangaCard(
+                            manga = manga,
+                            onCardClick = { navigateToManga(manga.id) },
                         )
+                        IconButton(
+                            onClick = { onEvent(MangaHistoryEvent.DeleteHistoryItem(manga.id)) },
+                        ) {
+                            Icon(
+                                Icons.Filled.Delete,
+                                contentDescription = null,
+                                tint = MaterialTheme.colorScheme.error
+                            )
+                        }
                     }
                 }
-            }
 
-            if (state.canLoadMore) {
-                item {
-                    Loading(
-                        modifier = Modifier.fillMaxWidth(),
-                        size = 32
-                    )
-                    LaunchedEffect(true) {
-                        onEvent(MangaHistoryEvent.LoadMore)
+                if (state.canLoadMore) {
+                    item {
+                        Loading(
+                            modifier = Modifier.fillMaxWidth(),
+                            size = 32
+                        )
+                        LaunchedEffect(true) {
+                            onEvent(MangaHistoryEvent.LoadMore)
+                        }
                     }
                 }
             }
