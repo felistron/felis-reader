db3958dc9daa8c7fe050d0074733e3b3859b326e
Fernando Espinosa <jferespinosa18@gmail.com>
2025-01-27 18:30:34 -0600

    Add swipe to refresh in ChapterListScreen

--- app/src/main/java/com/felisreader/chapter/presentation/chapter_list/ChapterListEvent.kt
+++ app/src/main/java/com/felisreader/chapter/presentation/chapter_list/ChapterListEvent.kt
@@ -1,9 +1,10 @@
 package com.felisreader.chapter.presentation.chapter_list
 
-import com.felisreader.core.domain.model.OrderType
-
 sealed class ChapterListEvent {
-    data class FeedChapters(val mangaId: String, val order: OrderType = OrderType.Descending): ChapterListEvent()
+    data class FeedChapters(
+        val mangaId: String,
+        val callback: suspend () -> Unit = {},
+    ): ChapterListEvent()
     object LoadMore: ChapterListEvent()
     object ToggleOrder: ChapterListEvent()
 }
